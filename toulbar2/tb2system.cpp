e0f4faf7d0d52a3ee81182f5aa30691e2618cda3
Simon de Givry <Simon.Degivry@toulouse.inra.fr>
2005-11-23 17:08:00 +0000

    inline cost2log2

--- toulbar2/tb2system.cpp
+++ toulbar2/tb2system.cpp
@@ -4,11 +4,3 @@
 
 #include "tb2system.hpp"
 
-int cost2log2(int v)
-{ 
-  float x;
-
-  if (v==0) return -1;
-  x=(float) v; 
-  return (*(int*)&x >> 23) - 127;
-}
--- toulbar2/tb2system.hpp
+++ toulbar2/tb2system.hpp
@@ -6,6 +6,13 @@
 #ifndef TB2SYSTEM_HPP_
 #define TB2SYSTEM_HPP_
 
-int cost2log2(int v);
+inline int cost2log2(int v)
+{ 
+  float x;
+
+  if (v==0) return -1;
+  x=(float) v; 
+  return (*(int*)&x >> 23) - 127;
+}
 
 #endif /* TB2SYSTEM_HPP_ */
