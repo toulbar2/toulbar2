f1679cb99f28e689dba3ae0bbe55399a8c6e2ad8
Simon de Givry <Simon.Degivry@toulouse.inra.fr>
2006-10-09 10:44:42 +0000

    new command line format. toulbar2 filename upperbound options

--- toulbar2/tb2main.cpp
+++ toulbar2/tb2main.cpp
@@ -8,28 +8,42 @@
 int main(int argc, char **argv)
 {
     if (argc <= 1) {
-        cerr << "Missing a wcsp filename as first argument!" << endl;
-        cerr << argv[0] << " filename [upperbound(" << MAX_COST << ") [verbosity(" << ToulBar2::verbose
-             << ") [storesize(" << STORE_SIZE << ") [options]]]]" << endl;
-        cerr << "Available options are:" << endl;
+        cerr << "Missing a problem filename as first argument!" << endl;
+        cerr << endl;
+        cerr << argv[0] << " filename upperbound options" << endl;
+        cerr << endl;
+        cerr << "Available problem formats (specified by the filename extension):" << endl;
+        cerr << "   *.wcsp : wcsp format" << endl;
+        cerr << "   *.pre  : pedigree format" << endl;
+        cerr << endl;
+        cerr << "Initial upperbound is optional (default value: " << MAX_COST << ")" << endl;
+        cerr << endl;
+        cerr << "Available options:" << endl;
+        cerr << "   v : verbosity (repeat this option to increase the verbosity level)" << endl;
+        cerr << "   s : show each solution found" << endl;
         cerr << "   b : binary branching always (default: binary branching for interval domain and n-ary branching for enumerated domain)" << endl;
-        cerr << "   e : boosting search with variable elimination of small degree (<2)" << endl;
+        cerr << "   e : boosting search with variable elimination of small degree (less than or equal to 2)" << endl;
+        cerr << "   p : preprocessing only: variable elimination of small degree (less than or equal to 2)" << endl;
+        cerr << "   t : preprocessing only: project ternary constraints on binary constraints" << endl;
+        cerr << endl;
         exit(EXIT_FAILURE);
     } 
-
-    if (argc >= 4) ToulBar2::verbose = atoi(argv[3]);
-    if (ToulBar2::verbose >= 1) ToulBar2::showSolutions = true;
-    if (argc >= 6 && strchr(argv[5],'b')) ToulBar2::binaryBranching = true;
-    if (strstr(argv[1],".pre")) ToulBar2::pedigree = new Pedigree;
-
-	int storesize = (argc >= 5)?atoi(argv[4]):STORE_SIZE;
+    
+    ToulBar2::verbose = 0;
+    for (int i=2; i<argc; i++) {
+        for (int j=0; argv[i][j] != 0; j++) if (argv[i][j]=='v') ToulBar2::verbose++;
+        if (strchr(argv[i],'s')) ToulBar2::showSolutions = true;
+        if (strchr(argv[i],'b')) ToulBar2::binaryBranching = true;
+        if (strchr(argv[i],'e')) ToulBar2::elimVarWithSmallDegree = true;
+        if (strchr(argv[i],'p')) { ToulBar2::elimVarWithSmallDegree = true; ToulBar2::only_preprocessing = true; }
+        if (strchr(argv[i],'t')) ToulBar2::preprocessTernary = true;
+    }
 	Cost c = (argc >= 3)?(Cost) atoi(argv[2]):MAX_COST; 	
-    Solver solver(storesize, c);
+    Solver solver(STORE_SIZE, (c>0)?c:MAX_COST);
 
     try {
+        if (strstr(argv[1],".pre")) ToulBar2::pedigree = new Pedigree;
         solver.read_wcsp(argv[1]);
-        if (argc >= 6 && strchr(argv[5],'e')) ToulBar2::elimLevel = 2;
-        if (argc >= 6 && strchr(argv[5],'p')) { ToulBar2::elimLevel = 2; ToulBar2::only_preprocessing = true; }
         solver.solve();
     } catch (Contradiction) {
         cout << "No solution found by initial propagation!" << endl;
