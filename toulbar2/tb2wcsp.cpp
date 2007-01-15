a2785ce3554b79237bb591a2e74df212a56088bd
Simon de Givry <Simon.Degivry@toulouse.inra.fr>
2007-01-15 08:27:13 +0000

    add an overflow test in Prob2Cost method.

--- toulbar2/tb2wcsp.cpp
+++ toulbar2/tb2wcsp.cpp
@@ -661,11 +661,15 @@ Constraint* WCSP::sum( Constraint* ctr1, Constraint* ctr2  )
 
 Cost WCSP::Prob2Cost(TProb p) const {
 	if (p == 0.0) return getUb();
-	Cost c = -Log10(p)*ToulBar2::NormFactor;
+    TProb res = -Log10(p)*ToulBar2::NormFactor;
+    if (res > (TProb) MAX_COST) {
+      cerr << "Overflow when converting probability to cost." << endl;
+      abort();
+    }
+	Cost c = (Cost) res;
 	if(c > getUb()) return getUb();
 	return c;
 }
 
 TProb WCSP::Cost2LogLike(Cost c) const { return -(TProb)c/ToulBar2::NormFactor; }
 TProb WCSP::Cost2Prob(Cost c) const { return Pow((TProb)10., -(TProb)c/ToulBar2::NormFactor); }
- 
