891b450594888f338ffed5266665530fc8fba87b
David Allouche <allouche@toulouse.inra.fr>
2009-12-08 17:32:19 +0000

    example of test tuning

---
+++ toulbar2/validation/test-opt.cmake
@@ -0,0 +1,8 @@
+# argument in command line
+set (command_line_option "B0 -v :e" )  
+# test timeout ( used for all wcsp founded in the directory
+set (test_timeout 100)
+#regexp to define successfull end.
+set (test_regexp  "Optimum: ${UB}")
+
+#regex error can also be define: ...add set_test_propertie in test.cmake ...to be done
