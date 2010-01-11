a5cd84207ae9db5223e8849c1e6b1df075a5abe0
David Allouche <allouche@toulouse.inra.fr>
2010-01-11 15:24:35 +0000

    test enumeration

---
+++ toulbar2/validation/enum/test-opt.cmake
@@ -0,0 +1,8 @@
+# argument in command line
+set (command_line_option "B1 a:e k1" )  
+# test timeout ( used for all wcsp founded in the directory
+set (test_timeout 100)
+#regexp to define successfull end.
+set (test_regexp  ${ENUM})
+
+#regex error can also be define: ...add set_test_propertie in test.cmake ...to be done
