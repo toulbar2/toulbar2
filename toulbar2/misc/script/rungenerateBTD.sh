a4423d43f8b850f43c615a9f71eb123eaa46eda3
Simon de Givry <Simon.Degivry@toulouse.inra.fr>
2010-02-23 16:18:42 +0000

    test file for BTD-like methods now exploits an MCS tree decomposition.

--- toulbar2/misc/script/rungenerateBTD.sh
+++ toulbar2/misc/script/rungenerateBTD.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
 nerr=0
-ninstances=50
-nend=100
+ninstances=100
+nend=10
 bctr=10
 tctr=1
 nary=1
-tight=30
+tight=40
 n=6
-m=10
+d=5
 K=100
 
 while (( $n < $nend )) ; do
   seed=0
-  echo n:$n m:$m tight:$tight%  binary:$bctr  ternary:$tctr  quatary:$nary  
+  echo n:$n m:$d tight:$tight%  binary:$bctr  ternary:$tctr  quatary:$nary  
   while (( $seed < $ninstances )) ; do
     rm -f toulbar2_opt
     rm -f toolbar_opt
+    rm -f toolbar_sol
     rm -f order
-    randomfile=nary-$n-$m-$tight-$bctr-$tctr-$nary-$seed 
+    rm -f sol
+    randomfile=nary-$n-$d-$tight-$bctr-$tctr-$nary-$seed 
     toulbar2 $randomfile C$K z > /dev/null
-    ./bin/Linux/toulbar2 problem.wcsp $1 w | awk 'BEGIN{opt="-";} /^Optimum: /{opt=$2;}  END{printf("%d ",opt); }'   > toulbar2_opt
+    ./misc/bin/Linux/peo problem.wcsp 1 > order
+    ./bin/Linux/toulbar2 problem.wcsp $1 w Oorder | awk 'BEGIN{opt="-";} /^Optimum: /{opt=$2;}  END{printf("%d ",opt); }'   > toulbar2_opt
     toolbar problem.wcsp  | awk 'BEGIN{opt="-";} /^Optimum: /{opt=$2;}  END{printf("%d \n",opt); }' > toolbar_opt
     toolbar problem.wcsp  -csol  | awk 'BEGIN{opt="-";} /^Total cost /{opt=$4;}  END{printf("%d \n",opt); }' > toolbar_sol
     ub1=`cat toulbar2_opt`
@@ -38,12 +41,14 @@ while (( $n < $nend )) ; do
     seed=`expr $seed + 1`
   done	
   nary=`expr $nary + 1` 
+  tctr=`expr $tctr + 1`  
   bctr=`expr $bctr + 10`  
-  n=`expr $n + 4`
+  n=`expr $n + 1`
 done
 
 rm -f problem.wcsp
 rm -f toulbar2_opt
 rm -f toolbar_opt
-  
- 
+rm -f toolbar_sol
+rm -f order
+rm -f sol
