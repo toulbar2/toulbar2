f3b347ed0385a1271174e233a094773fa1407a80
Simon de Givry <Simon.Degivry@toulouse.inra.fr>
2010-08-09 07:38:44 +0000

    global cost functions inclusion delayed

--- toulbar2/cmake-script/source_files.cmake
+++ toulbar2/cmake-script/source_files.cmake
@@ -16,20 +16,20 @@
                         ${My_Source}/tb2btqueue.cpp
                         ${My_Source}/tb2clusters.cpp
                         ${My_Source}/tb2constraint.cpp
-                        ${My_Source}/tb2graph.cpp
-                        ${My_Source}/tb2globalconstr.cpp
-                        ${My_Source}/tb2flowbasedconstr.cpp
-                        ${My_Source}/tb2alldiffconstr.cpp
-                        ${My_Source}/tb2globalcardinalityconstr.cpp
-                        ${My_Source}/tb2sameconstr.cpp
-                        ${My_Source}/tb2regularconstr.cpp
+#                        ${My_Source}/tb2graph.cpp
+#                        ${My_Source}/tb2globalconstr.cpp
+#                        ${My_Source}/tb2flowbasedconstr.cpp
+#                        ${My_Source}/tb2alldiffconstr.cpp
+#                        ${My_Source}/tb2globalcardinalityconstr.cpp
+#                        ${My_Source}/tb2sameconstr.cpp
+#                        ${My_Source}/tb2regularconstr.cpp
                         ${My_Source}/tb2domain.cpp
                         ${My_Source}/tb2enumvar.cpp
                         ${My_Source}/tb2intervar.cpp
                         ${My_Source}/tb2main.cpp
                         ${My_Source}/tb2naryconstr.cpp
-                        ${My_Source}/tb2haplotype.cpp
                         ${My_Source}/tb2pedigree.cpp
+                        ${My_Source}/tb2haplotype.cpp
                         ${My_Source}/tb2queue.cpp
                         ${My_Source}/tb2randomgen.cpp
                         ${My_Source}/tb2reader.cpp
@@ -62,19 +62,19 @@ MESSAGE(STATUS "-------------\n")
                         ${My_Source}/tb2btqueue.*pp
                         ${My_Source}/tb2clusters.*pp
                         ${My_Source}/tb2constraint.*pp
-                        ${My_Source}/tb2graph.*pp
-                        ${My_Source}/tb2globalconstr.*pp
-                        ${My_Source}/tb2flowbasedconstr.*pp
-                        ${My_Source}/tb2alldiffconstr.*pp
-                        ${My_Source}/tb2globalcardinalityconstr.*pp
-                        ${My_Source}/tb2sameconstr.*pp
-                        ${My_Source}/tb2regularconstr.*pp
+#                        ${My_Source}/tb2graph.*pp
+#                        ${My_Source}/tb2globalconstr.*pp
+#                        ${My_Source}/tb2flowbasedconstr.*pp
+#                        ${My_Source}/tb2alldiffconstr.*pp
+#                        ${My_Source}/tb2globalcardinalityconstr.*pp
+#                        ${My_Source}/tb2sameconstr.*pp
+#                        ${My_Source}/tb2regularconstr.*pp
                         ${My_Source}/tb2domain.*pp
                         ${My_Source}/tb2enumvar.*pp
                         ${My_Source}/tb2intervar.*pp
                         ${My_Source}/tb2naryconstr.*pp
-                        ${My_Source}/tb2haplotype.*pp
                         ${My_Source}/tb2pedigree.*pp
+                        ${My_Source}/tb2haplotype.cpp
                         ${My_Source}/tb2queue.*pp
                         ${My_Source}/tb2randomgen.*pp
                         ${My_Source}/tb2reader.*pp
