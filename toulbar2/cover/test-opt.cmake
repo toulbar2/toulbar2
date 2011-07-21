acf5296ab58ec0046210bd5a6e042d870fbfd5c2
David Allouche <allouche@toulouse.inra.fr>
2011-07-21 09:42:45 +0000

    cover test adding

---
+++ toulbar2/cover/test-opt.cmake
@@ -0,0 +1,8 @@
+# list of arguments use in command line for the current directory
+# test timeout ( used for all wcsp founded in the directory
+set (test_timeout 20)
+#regexp to define successfull end.
+#set (test_regexp  "Optimum: ${UB}")
+set (test_regexp  "end." )
+
+#regex error can also be define: ...add set_test_propertie in test.cmake ...to be done
