6523f6cb2033ad156dfafd672880ab8e4989251d
David Allouche <allouche@toulouse.inra.fr>
2015-01-29 13:36:25 +0000

    [misc] solution checking for global contraint test case

---
+++ toulbar2/validation/decomposable/test-opt.cmake
@@ -0,0 +1,8 @@
+# list of arguments use in command line for the current directory
+set (command_line_option -v -s )  
+# test timeout ( used for all wcsp founded in the directory
+set (test_timeout 100)
+#regexp to define successfull end.
+set (test_regexp  "Optimum: ${UB}")
+
+#regex error can also be define: ...add set_test_propertie in test.cmake ...to be done
