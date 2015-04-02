15fee93f9b8925362ab659c1e03a7506b6e55ee6
Thomas Schiex <Thomas.Schiex@toulouse.inra.fr>
2015-04-02 11:24:08 +0200

    [misc] final excision of peo and narycsp/incop binary files

--- toulbar2/misc/script/td.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Provides a variable elimination ordering to ToulBar2
-
-# Usage: td.sh problem.wcsp toulbar2_options
-
-# by default, uses Maximum Cardinality Search heuristic
-peo $1 1 > order
-toulbar2 $1 $2 Oorder
