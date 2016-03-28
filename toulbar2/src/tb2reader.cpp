745fedc1a9abef44ee20c59e2b31b5ccf5b4092a
Thomas Schiex <Thomas.Schiex@toulouse.inra.fr>
2016-03-28 21:03:53 +0200

    [misc] New macro DEBUGONLY that removes code when NDEBUG is defined.
    Used to remove warnings.
    Could be improved to be indentation transparent ?

--- toulbar2/src/tb2reader.cpp
+++ toulbar2/src/tb2reader.cpp
@@ -365,10 +365,7 @@ void WCSP::read_wcsp(const char *fileName)
         file >> domsize;
         if (domsize > nbvaltrue) nbvaltrue = domsize;
         if (ToulBar2::verbose >= 3) cout << "read variable " << i << " of size " << domsize << endl;
-        int theindex = -1;
-
-        if (domsize >= 0) theindex = makeEnumeratedVariable(varname, 0, domsize - 1);
-        else theindex = makeIntervalVariable(varname, 0, -domsize - 1);
+        DEBONLY(int theindex = ) ((domsize >= 0) ? makeEnumeratedVariable(varname,0,domsize-1) : makeIntervalVariable(varname,0,-domsize-1));
         assert(theindex == i);
     }
 
@@ -838,9 +835,7 @@ void WCSP::read_uai2008(const char *fileName)
         file >> domsize;
         if (ToulBar2::verbose >= 3) cout << "read variable " << i << " of size " << domsize << endl;
         if (domsize > nbval) nbval = domsize;
-        int theindex = -1;
-        if (domsize >= 0) theindex = makeEnumeratedVariable(varname, 0, domsize - 1);
-        else theindex = makeIntervalVariable(varname, 0, -domsize - 1);
+        DEBONLY(int theindex =) ((domsize >= 0)  ? makeEnumeratedVariable(varname,0,domsize-1) : makeIntervalVariable(varname,0,-domsize-1));
         assert(theindex == i);
     }
 
@@ -1287,8 +1282,7 @@ void WCSP::read_wcnf(const char *fileName)
     for (int i = 0; i < nbvar; i++) {
         string varname;
         varname = to_string(i);
-        int theindex = -1;
-        theindex = makeEnumeratedVariable(varname, 0, 1);
+        DEBONLY(int theindex = ) makeEnumeratedVariable(varname,0,1);
         assert(theindex == i);
     }
 
