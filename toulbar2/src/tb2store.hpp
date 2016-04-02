42fa5398e0a2b3cc92215b2295825b481f0b81f3
Thomas Schiex <Thomas.Schiex@toulouse.inra.fr>
2016-04-02 16:12:13 +0200

    [misc] Nicer type names with boost
    Store allocations printed by default (verbose = 0)

--- toulbar2/src/tb2store.hpp
+++ toulbar2/src/tb2store.hpp
@@ -25,7 +25,7 @@
 #define TB2STORE_HPP_
 
 #include "tb2types.hpp"
-#include <typeinfo>
+#include <boost/type_index.hpp>
 
 template<class T> class BTList;
 template<class T> class DLink;
@@ -62,8 +62,8 @@ public:
         content = new V[indexMax];
         index = 0;
         base = 0;
-        if (ToulBar2::verbose > 0) {
-            cout << "c " << indexMax * (sizeof(V) + sizeof(T *)) << " Bytes allocated for " << typeid(T).name() << " stack." << endl;
+        if (ToulBar2::verbose >= 0) {
+            cout << "c " << indexMax * (sizeof(V) + sizeof(T *)) << " Bytes allocated for " << boost::typeindex::type_id<T>().pretty_name() << " stack." << endl;
         }
     }
 
