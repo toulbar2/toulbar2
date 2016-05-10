a486625264f7966716399ed865ef03f0ce3344c8
Thomas Schiex <Thomas.Schiex@toulouse.inra.fr>
2016-05-10 16:33:10 +0200

    [misc] doc target attached to "all" and extra time for cover test (for slow platforms)

--- toulbar2/cmake-script/UseDoxygen.cmake
+++ toulbar2/cmake-script/UseDoxygen.cmake
@@ -29,13 +29,43 @@
 #  DOXYFILE_HTML_DIR - Directory where the Doxygen html output is stored. Defaults to "html".
 #
 
-#
-#  Copyright (c) 2009 Tobias Rautenkranz <tobias@rautenkranz.ch>
-#
-#  Redistribution and use is allowed according to the terms of the New
-#  BSD license.
-#  For details see the accompanying COPYING-CMAKE-SCRIPTS file.
-#
+IF (DOXYGEN_FOUND)
+  MESSAGE(STATUS "########## package doxygen found #######################")
+ELSE(DOXYGEN_FOUND)
+  MESSAGE(STATUS "######### doxygen not found. Cannot generate doc...#############")
+ENDIF (DOXYGEN_FOUND)
+
+set(doxyfile_in ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in)
+set(doxyfile ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
+# output directory for code source documentation  
+
+configure_file(${doxyfile_in} ${doxyfile} @ONLY)
+
+add_custom_target(doc ALL
+  DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/doxygen.stamp)
+
+add_custom_command(
+  OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/doxygen.stamp
+  DEPENDS ${doxyfile}
+  COMMAND ${DOXYGEN_EXECUTABLE} ${doxyfile}
+  COMMAND cmake -E touch ${CMAKE_CURRENT_BINARY_DIR}/doxygen.stamp
+  WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+  COMMENT "Generating API documentation with Doxygen"
+  VERBATIM)
+
+if(BUILD_API_DOC_LATEX STREQUAL "ON")
+  set(DOXYFILE_GENERATE_LATEX "YES")
+  find_program(DOXYFILE_MAKE make)
+  mark_as_advanced(DOXYFILE_MAKE)
+  if(LATEX_COMPILER AND MAKEINDEX_COMPILER AND DOXYFILE_MAKE)
+    if(PDFLATEX_COMPILER)
+      set(DOXYFILE_PDFLATEX "YES")
+    endif()
+    if(DOXYGEN_DOT_EXECUTABLE)
+      set(DOXYFILE_DOT "YES")
+    endif()
+endif()
+endif()
 
 macro(usedoxygen_set_default name value)
 	if(NOT DEFINED "${name}")
--- toulbar2/cover/test-opt.cmake
+++ toulbar2/cover/test-opt.cmake
@@ -1,6 +1,6 @@
 # list of arguments use in command line for the current directory
 # test timeout ( used for all wcsp found in the directory
-set (test_timeout 20)
+set (test_timeout 300)
 #regexp to define successfull end.
 #set (test_regexp  "Optimum: ${UB}")
 set (test_regexp  "end." )
