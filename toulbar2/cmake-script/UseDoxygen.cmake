fdbce0e33f223f602e5f26abcc4b1745ed1c5be7
Thomas Schiex <Thomas.Schiex@toulouse.inra.fr>
2016-05-10 16:05:24 +0200

    [doc] change CMake doc generation.
    	- Builds HTML and optionally PDF from latex.
    	- Controlled by just two Cmake variables (BUILD_API_DOC and BUILD_API_DOC_LATEX)

--- toulbar2/cmake-script/UseDoxygen.cmake
+++ toulbar2/cmake-script/UseDoxygen.cmake
@@ -1,23 +1,52 @@
 # add a target to generate API documentation with Doxygen
 find_package(LATEX)
-option(BUILD_DOCUMENTATION "Create and install the HTML based API documentation (requires Doxygen)" ${DOXYGEN_FOUND})
+find_package(Doxygen)
 
-if(BUILD_DOCUMENTATION)
-    if(NOT DOXYGEN_FOUND)
-        message(FATAL_ERROR "Doxygen is needed to build the documentation.")
+IF (DOXYGEN_FOUND)
+  MESSAGE(STATUS "########## package doxygen found #######################")
+ELSE(DOXYGEN_FOUND)
+  MESSAGE(STATUS "######### doxygen not found. Cannot generate doc...#############")
+ENDIF (DOXYGEN_FOUND)
+
+set(doxyfile_in ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in)
+set(doxyfile ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
+# output directory for code source documentation  
+
+configure_file(${doxyfile_in} ${doxyfile} @ONLY)
+
+add_custom_target(doc
+  COMMAND ${DOXYGEN_EXECUTABLE} ${doxyfile}
+  WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+  COMMENT "Generating API documentation with Doxygen"
+  VERBATIM)
+
+if(BUILD_API_DOC_LATEX STREQUAL "ON")
+  set(DOXYFILE_GENERATE_LATEX "YES")
+  find_program(DOXYFILE_MAKE make)
+  mark_as_advanced(DOXYFILE_MAKE)
+  if(LATEX_COMPILER AND MAKEINDEX_COMPILER AND DOXYFILE_MAKE)
+    if(PDFLATEX_COMPILER)
+      set(DOXYFILE_PDFLATEX "YES")
     endif()
-
-    set(doxyfile_in ${CMAKE_CURRENT_SOURCE_DIR}/Doxyfile.in)
-    set(doxyfile ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile)
-
-    configure_file(${doxyfile_in} ${doxyfile} @ONLY)
-
-    add_custom_target(doc
-        COMMAND ${DOXYGEN_EXECUTABLE} ${doxyfile}
-        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-        COMMENT "Generating API documentation with Doxygen"
-        VERBATIM)
-      
-    install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/html DESTINATION share/doc)
+    if(DOXYGEN_DOT_EXECUTABLE)
+      set(DOXYFILE_DOT "YES")
+    endif()
+    
+    add_custom_command(TARGET doc
+      POST_BUILD
+      COMMAND ${DOXYFILE_MAKE}
+      COMMENT "Running LaTeX for Doxygen documentation in ${CMAKE_CURRENT_BINARY_DIR}/latex..."
+      WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}/latex")
+    install(FILES ${CMAKE_CURRENT_BINARY_DIR}/latex/refman.pdf DESTINATION ${doc_destination}/${Toulbar_NAME_COMPLETE})
+  else()
+    set(DOXYGEN_LATEX "NO")
+  endif()
+else()
+  set(DOXYFILE_GENERATE_LATEX "NO")
 endif()
+
+install(DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/html DESTINATION ${doc_destination}/${Toulbar_NAME_COMPLETE})
+
+
+
 
