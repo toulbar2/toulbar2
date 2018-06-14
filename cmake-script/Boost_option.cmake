0f7303d4817d4b4bd4ca25e17a13f57880ddf50c
Thomas Schiex <thomas.schiex@free.fr>
2018-06-14 10:41:01 +0200

    [misc] Boost iostream and zlib required for compressed cfn format..

--- cmake-script/Boost_option.cmake
+++ cmake-script/Boost_option.cmake
@@ -1,30 +1,21 @@
 #code executed if the boost option is add
 
-        FILE(
-                        GLOB
-                        boost_file      
-                        ${My_Source}/utils/tb2boostgraph.cpp
+FILE(GLOB boost_file ${My_Source}/tb2boostgraph.cpp)
 
-            )
-
-# list of cpp file extention 
+# list of cpp file extension 
 SET (source_files ${source_files} ${boost_file})
 
 
 # boost detection 
         MESSAGE(STATUS "- boost flag on  .")
+find_package(Boost 1.34.1 REQUIRED COMPONENTS graph iostreams)
+find_package(ZLIB)
 
-        find_package(
-                        Boost 
-                        1.34.1
-                        REQUIRED graph
-                    )
-
-SET (all_depends  ${all_depends} ${BOOST_LIBRARIES})
+SET (all_depends ${all_depends} Boost::iostreams ${BOOST_LIBRARIES} ${ZLIB_LIBRARIES})
 
 IF(NOT Boost_FOUND)
         MESSAGE(ERROR "#################################")
-        MESSAGE(ERROR "boost package not found")
+        MESSAGE(ERROR "Boost package not found")
         MESSAGE(ERROR "#################################")
 ELSE (NOT Boost_FOUND)
         MESSAGE(STATUS "boost Package configured successfully.")
