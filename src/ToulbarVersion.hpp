ee72a61dd4bfa10ace76f83266558e0364f7b56f
Simon de Givry <degivry@toulouse.inra.fr>
2019-02-04 16:11:50 +0100

    version

--- src/ToulbarVersion.hpp
+++ src/ToulbarVersion.hpp
@@ -1,2 +1,2 @@
 // Cmake generated version
-#define Toulbar_VERSION "1.0.0-94-gc385bda-fural/strictac-tainted (1549291741)"
+#define Toulbar_VERSION "1.0.0-95-g7709a60-fural/strictac-tainted (1549292310)"
