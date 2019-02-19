84e7c99e5832afd2b09cfb68ff65fe9d873c2468
Fulya Ural <fulya.ural@inra.fr>
2019-02-19 15:43:25 +0100

    [misc] Updating version files.

--- src/ToulbarVersion.hpp
+++ src/ToulbarVersion.hpp
@@ -1,2 +1,2 @@
 // Cmake generated version
-#define Toulbar_VERSION "1.0.0-106-g9fd5cd3-fural/strictac-tainted (1550587395)"
+#define Toulbar_VERSION "1.0.0-107-g41de4af-fural/strictac-tainted (1550587405)"
