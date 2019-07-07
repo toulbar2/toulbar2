f5c48fbe61a37222a3aa6e1001df867575d305d4
kad15 <kader.beldjilali@free.fr>
2019-07-07 10:04:12 +0200

    master-worker

--- kad_maj_git.sh
+++ kad_maj_git.sh
@@ -5,4 +5,3 @@ git commit -m "master-worker" 1>/dev/null 2>&1
 git push origin kad 1>/dev/null 2>&1
 git push origin kad 1>/dev/null 2>&1
 git push origin kad | grep "Everything up-to-date"
-sleep 1s
