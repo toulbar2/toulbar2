a578aee79991fb164306c6cdeb959c07a62516cc
kad <kader.beldjilali@free.fr>
2019-07-23 17:43:27 +0200

    [misc] Updating version files.

--- src/ToulbarVersion.hpp
+++ src/ToulbarVersion.hpp
@@ -1,6 +1,2 @@
 // Cmake generated version
-<<<<<<< HEAD
-#define Toulbar_VERSION "1.0.1-297-gf6fd6f4-kad (1563896575)"
-=======
-#define Toulbar_VERSION "1.0.1-297-g61fd331-kad (1563895608)"
->>>>>>> c6b2e5a86cbbef53bac9c04c30f1af5e6f661cf5
+#define Toulbar_VERSION "1.0.1-301-ga81ca95-kad (1563896607)"
