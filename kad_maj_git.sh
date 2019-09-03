e101051b84384338f3bfaeeece222fef1a7745cb
kad15 <kader.beldjilali@free.fr>
2019-09-03 23:20:19 +0200

    maj script kad_maj_git.sh

--- kad_maj_git.sh
+++ kad_maj_git.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-git pull origin kad 1>/dev/null 2>&1
+git pull origin kad2 1>/dev/null 2>&1
 git add . 1>/dev/null 2>&1
 git commit -m "master-worker" 1>/dev/null 2>&1
-git push origin kad 1>/dev/null 2>&1
-git push origin kad 1>/dev/null 2>&1
-git push origin kad | grep "Everything up-to-date"
-sleep 4s
+git push origin kad2 1>/dev/null 2>&1
+git push origin kad2 1>/dev/null 2>&1
+git push origin kad2 | grep "Everything up-to-date"
+sleep 2s
