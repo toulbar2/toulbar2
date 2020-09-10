ba1bff8b6d5a2c5154a88706166a277c38fd2a10
Simon de Givry <degivry@toulouse.inra.fr>
2020-09-10 14:00:40 +0200

    [web] update warehouse tutorial

--- web/TUTORIALS/warehouse.py
+++ web/TUTORIALS/warehouse.py
@@ -1,71 +1,134 @@
 
 import sys
-import CFN
 
-f = open(sys.argv[1], 'r').readlines()
+def flatten(x):
+    result = []
+    for el in x:
+        if hasattr(el, "__iter__") and not isinstance(el, str) and not isinstance(el, tuple) and not isinstance(el, dict):
+            result.extend(flatten(el))
+        else:
+            result.append(el)
+    return result
 
-precision = int(sys.argv[2])  # used to convert cost values from float to integer
+def cfn(problem, isMinimization, initPrimalBound, floatPrecision=0):
+    globals_key_order = ["metric", "cost", "bounds", "vars1", "vars2", "nb_states", "starts", "ends", "transitions", "nb_symbols", "nb_values", "start", "terminals", "non_terminals", "min", "max", "values", "defaultcost", "tuples", "comparator", "to"]
+    print('{')
+    print('\tproblem: { "name": "%s", "mustbe": "%s%.*f" },' % (problem["name"], "<" if (isMinimization) else ">", floatPrecision, initPrimalBound))
+    print('\tvariables: {', end='')
+    for i,e in enumerate(flatten(problem["variables"])):
+        if i > 0: print(', ', end='')
+        print('"%s":' % e[0], end='')
+        if isinstance(e[1], int):
+            print(' %s' % e[1], end='')
+        else:
+            print('[', end='')
+            for j,a in enumerate(e[1]):
+                if j > 0: print(', ', end='')
+                print('"%s"' % a, end='')
+            print(']', end='')
+    print('},')
+    print( '\tfunctions: {')
+    for i,e in enumerate(flatten(problem["functions"])):
+        if i > 0: print(',')
+        if e.get("name") is not None: print('\t\t"%s": {scope: [' % e.get("name"), end='')
+        else: print('\t\t{scope: [', end='')
+        scope = {}
+        for j,x in enumerate(e.get("scope")):
+            if (x in scope): sys.exit(str(e) + '\nError: scope of function ' + str(i) + ' with the same variable twice is forbidden!')
+            if j > 0: print(', ', end='')
+            print('"%s"' % x, end='')
+            scope[x]=j
+        print('], ', end='')
+        if e.get("type") is not None:
+            print('"type": %s, ' % e.get("type"), end='')
+        if e.get("params") is not None:
+            if isinstance(e.get("params"), dict):
+                print('"params": {', end='')
+                first = True
+                for key in globals_key_order:
+                    if key in e.get("params"):
+                        if not first: print(', ', end='')
+                        if isinstance(e.get("params")[key], str): print('"%s": "%s"' % (str(key),str(e.get("params")[key]).replace("'", '"')), end='')
+                        else: print('"%s": %s' % (str(key),str(e.get("params")[key]).replace("'", '"')), end='')
+                        first = False
+                print ('}', end='')
+            else: print('"params": %s, ' % str(e.get("params")).replace("'",'"'), end='')
+        if e.get("defaultcost") is not None:
+            print('"defaultcost:" %s, ' % e.get("defaultcost"), end='')
+        if e.get("costs") is not None:
+            print('"costs": ', end='')
+            if isinstance(e.get("costs"), str):
+                print('"%s"' % e.get("costs"), end='') # reuse future cost function by giving its name here
+            else:
+                print('[', end='')
+                for j,c in enumerate(e.get("costs")):
+                    if j > 0: print(', ', end='')
+                    if isinstance(c, str) and not c.isdigit():
+                        print('"%s"' % c, end='')
+                    else:
+                        print('%s' % c, end='')
+                print(']', end='')
+        print('}', end='')
+    print('}\n}')
 
-tokens = []
-for l in f:
-    tokens += l.split()
+class Data:
+    def __init__(self, filename):
+        lines = open(filename).readlines()
+        tokens = flatten([[e for e in l.split()] for l in lines])
+        p = 0
+        self.n = int(tokens[p])
+        p += 1
+        self.m = int(tokens[p])
+        p += 1
+        self.top = 1.  # sum of all costs plus one
+        self.CostW = []  # maintenance cost of warehouses
+        self.Capacity = []  # capacity limit of warehouses (not used)
+        for i in range(self.n):  
+            self.Capacity.append(int(tokens[p]))
+            p += 1
+            self.CostW.append(float(tokens[p]))
+            p += 1
+        self.top += sum(self.CostW)
+        self.Demand = []  # demand for each store (not used)
+        self.CostS = []  # supply cost matrix
+        for j in range(self.m):
+            self.Demand.append(int(tokens[p]))
+            p += 1
+            self.CostS.append([])
+            for i in range(self.n):
+                self.CostS[j].append(float(tokens[p]))
+                p += 1
+            self.top += sum(self.CostS[-1])
 
-pos = 0
+def model(data):
+    Warehouse = ["w" + str(i) for i in range(data.n)]
+    Store = ["s" + str(i) for i in range(data.m)]
+    Model = {
+        "name": "Warehouse_" + str(data.n) + "_" + str(data.m),
+        "variables": [[(e, 2) for e in Warehouse],
+                      [(e, data.n)  for e in Store]],
+        "functions":
+            [
+                # maintenance costs
+                [{"scope": [Warehouse[i]],
+                  "costs": [0, data.CostW[i]]}
+                 for i in range(data.n)],
+                # supply costs
+                [{"scope": [Store[i]],
+                  "costs": data.CostS[i]}
+                 for i in range(data.m)],
+                # channeling constraints between warehouses and stores
+                [{"scope": [Warehouse[i], Store[j]],
+                  "costs": [(data.top if (a == 0 and b == i) else 0) for a in range(2) for b in range(data.n)]}
+                 for i in range(data.n) for j in range(data.m)]
+            ]
+    }
+    return Model
 
-
-def token():
-    global pos, tokens
-    if pos == len(tokens):
-        return None
-    s = tokens[pos]
-    pos += 1
-    return s
-
-
-N = int(token())  # number of warehouses
-M = int(token())  # number of stores
-
-top = 1  # sum of all costs plus one
-
-CostW = []  # maintenance cost of warehouses
-Capacity = []  # capacity limit of warehouses (not used)
-
-for i in range(N):  
-    Capacity.append(token())
-    CostW.append(int(float(token()) * 10.**precision))
-
-top += sum(CostW)
-
-Demand = []  # demand for each store
-CostS = [[] for i in range(M)]  # supply cost matrix
-
-for j in range(M):
-    Demand.append(int(token()))
-    for i in range(N):
-        CostS[j].append(int(float(token()) * 10.**precision))
-    top += sum(CostS[-1])
-
-# create a new empty cost function network
-Problem = CFN.CFN(top)
-# add warehouse variables
-for i in range(N):
-    Problem.AddVariable('w' + str(i), range(2))
-# add store variables
-for j in range(M):
-    Problem.AddVariable('s' + str(j), range(N))
-# add maintenance costs
-for i in range(N):
-    Problem.AddFunction([i], [0, CostW[i]])
-# add supply costs for each store
-for j in range(M):
-    Problem.AddFunction([N+j], CostS[j])
-# add channeling constraints between warehouses and stores
-for i in range(N):
-    for j in range(M):
-        Problem.AddFunction([i, N+j], [(top if (a == 0 and b == i) else 0) for a in range(2) for b in range(N)])
-
-Problem.Dump('warehouse.cfn')
-Problem.Option.FullEAC = False
-Problem.Option.showSolutions = False
-Problem.Solve()
+if __name__ == '__main__':
+    # read parameters
+    if len(sys.argv) < 2: exit('Command line argument is problem data filename and number of precision digits after the floating point')
+    data = Data(sys.argv[1])
+    # dump problem into JSON .cfn format for minimization
+    cfn(model(data), True, data.top, int(sys.argv[2]))
 
