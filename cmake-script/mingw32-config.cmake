cb342226cb29e9361ee4159e4cefa73fa262a267
Simon de Givry <degivry@toulouse.inra.fr>
2020-10-15 15:14:23 +0200

    [misc] new version 1.1.1 and cross compilation parameters

--- cmake-script/mingw32-config.cmake
+++ cmake-script/mingw32-config.cmake
@@ -3,14 +3,14 @@
         SET(CMAKE_SYSTEM_VERSION 1)
 
 #setup of cross compilator location
-        SET(CMAKE_C_COMPILER /usr/bin/i586-mingw32msvc-gcc)
-        SET(CMAKE_CXX_COMPILER /usr/bin/i586-mingw32msvc-c++)
+        SET(CMAKE_C_COMPILER /usr/bin/x86_64-w64-mingw32-g++-win32)
+        SET(CMAKE_CXX_COMPILER /usr/bin/x86_64-w64-mingw32-g++-win32)
 
 #setup of cros compilation required library and other header file for the target#plateform
 #by default under ubuntu 
 # the second varibale correspond to the prefix variable 
 
-        SET(CMAKE_FIND_ROOT_PATH /usr/i586-mingw32msvc ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/win32)
+        SET(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32 ${CMAKE_CURRENT_SOURCE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/win32)
 
 # search for programs in the build host directories
         SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
--- src/ToulbarVersion.hpp
+++ src/ToulbarVersion.hpp
@@ -1,2 +1,2 @@
 // Cmake generated version
-#define Toulbar_VERSION "1.0.1-197-gba1bff8-master-tainted (1599855642)"
+#define Toulbar_VERSION "1.0.1-202-g05d77ac-master-tainted (1602767143)"
