f7ed7de7e005ad826c7fe242ef2328721ed2c116
Thomas Schiex <thomas.schiex@free.fr>
2021-02-26 17:36:49 +0100

    [misc] Updated version file on push

--- src/ToulbarVersion.hpp
+++ src/ToulbarVersion.hpp
@@ -1,6 +1,2 @@
 // Cmake generated version
-<<<<<<< HEAD
-#define Toulbar_VERSION "1.1.1-34-g9227b0d-master-tainted (1614339934)"
-=======
-#define Toulbar_VERSION "1.1.1-33-gb643675b-master (1614356620)"
->>>>>>> 675cb467... [misc] Updated version file on push
+#define Toulbar_VERSION "1.1.1-37-gb924fa41-master (1614357409)"
