715f11955a1713a561f07a79d2bcb77277513066
Thomas Schiex <thomas.schiex@free.fr>
2021-04-24 21:30:53 +0200

    [pytb2] Travis automation building up 2

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ def read_version():
 
 setup(
     name='pytoulbar2',
-    version="0.0.0",
+    version="0.0.1",
     author='ToulBar2 team',
     author_email='thomas.schiex@inrae.fr',
     description='ToulBar2 Python module',
