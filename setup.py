0413388d61e91ac9d6d0d60eae9554443f96c657
Thomas Schiex <thomas.schiex@free.fr>
2021-04-28 21:32:22 +0200

    [pytb2] understanding macosx wheel building

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ def read_version():
 
 setup(
     name='pytoulbar2',
-    version="0.0.3",
+    version="0.0.3.1",
     author='ToulBar2 team',
     author_email='thomas.schiex@inrae.fr',
     description='ToulBar2 Python package',
