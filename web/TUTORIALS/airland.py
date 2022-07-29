9428afc5e408f794f67e8890476f10457695b111
Nathalie Rousse <nathalie.rousse@inra.fr>
2022-07-29 23:10:23 +0200

    [tuto] modify tuto_alp.rst tuto_bmp.rst and .py (with pytoulbar2)

--- web/TUTORIALS/airland.py
+++ web/TUTORIALS/airland.py
@@ -1,4 +1,3 @@
-
 import sys
 import pytoulbar2
 
@@ -11,12 +10,12 @@ for l in f:
 pos = 0
 
 def token():
-   global pos, tokens
-   if (pos == len(tokens)):
-      return None
-   s = tokens[pos]
-   pos += 1
-   return int(float(s)) 
+    global pos, tokens
+    if (pos == len(tokens)):
+        return None
+    s = tokens[pos]
+    pos += 1
+    return int(float(s))
 
 N = token()
 token() # skip freeze time
@@ -26,16 +25,16 @@ PC = []
 ST = []
 
 for i in range(N):
-   token()  # skip appearance time
+    token()  # skip appearance time
 # Times per plane: {earliest landing time, target landing time, latest landing time}
-   LT.append([token(), token(), token()])
+    LT.append([token(), token(), token()])
 
 # Penalty cost per unit of time per plane:
 # [for landing before target, after target]
-   PC.append([token(), token()])
+    PC.append([token(), token()])
 
 # Separation time required after i lands before j can land
-   ST.append([token() for j in range(N)])
+    ST.append([token() for j in range(N)])
 
 top = 99999
 
@@ -44,13 +43,26 @@ for i in range(N):
     Problem.AddVariable('x' + str(i), range(LT[i][0],LT[i][2]+1))
 
 for i in range(N):
-    Problem.AddFunction([i], [PC[i][0]*abs(a-LT[i][1]) for a in range(LT[i][0], LT[i][2]+1)])
+    ListCost = []
+    for a in range(LT[i][0], LT[i][2]+1):
+        if a < LT[i][1]:
+            ListCost.append(PC[i][0]*(LT[i][1] - a))
+        else:
+            ListCost.append(PC[i][1]*(a - LT[i][1]))
+    Problem.AddFunction([i], ListCost)
 
 for i in range(N):
     for j in range(i+1,N):
-        Problem.AddFunction([i, j], [top*(a+ST[i][j]>b and b+ST[j][i]>a) for a in range(LT[i][0], LT[i][2]+1) for b in range(LT[j][0], LT[j][2]+1)])
+        Constraint = []
+        for a in range(LT[i][0], LT[i][2]+1):
+            for b in range(LT[j][0], LT[j][2]+1):
+                if a+ST[i][j]>b and b+ST[j][i]>a:
+                    Constraint.append(top)
+                else:
+                    Constraint.append(0)
+        Problem.AddFunction([i, j],Constraint)
 
-Problem.Dump('airplane.cfn')
+#Problem.Dump('airplane.cfn')
 Problem.NoPreprocessing()
-Problem.Solve()
+Problem.Solve(showSolutions = 3)
 
