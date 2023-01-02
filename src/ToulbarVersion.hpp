ff39fbeaf9a1cc19cdef1398bb353459742b2c40
smbct <buchet.s@laposte.net>
2023-01-02 17:19:05 +0100

    [misc] Updated version file on push

--- src/ToulbarVersion.hpp
+++ src/ToulbarVersion.hpp
@@ -1,6 +1,2 @@
 // Cmake generated version
-<<<<<<< HEAD
-#define Toulbar_VERSION "v1.2.0-269-g49aec617-cccrit (1672675990)"
-=======
-#define Toulbar_VERSION "v1.2.0-258-ge3ee3dc4-cccrit (1670869231)"
->>>>>>> 7587b4fda64c66ae47f34d194c20ada5e0f844ba
+#define Toulbar_VERSION "v1.2.0-272-g00e0d07d-cccrit (1672676345)"
