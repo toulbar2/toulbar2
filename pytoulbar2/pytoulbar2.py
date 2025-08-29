98df2a76d3d652dbcf3516867142ec2428ec35f9
Simon de Givry <degivry@toulouse.inra.fr>
2025-08-29 11:31:47 +0200

    [python] minor corrections

--- pytoulbar2/pytoulbar2.py
+++ pytoulbar2/pytoulbar2.py
@@ -55,7 +55,7 @@ class CFN:
     """
     def __init__(self, ubinit = None, resolution = 0, vac = 0, configuration = False, vns = None, seed = 1, verbose = -1, init=True):
         if init:
-            tb2.reinit();
+            tb2.reinit()
         tb2.option.decimalPoint = resolution   # decimal precision of costs
         tb2.option.vac = vac   # if no zero, maximum search depth-1 where VAC algorithm is performed (use 1 for preprocessing only)
         tb2.option.seed = seed    # random seed number (use -1 if a pseudo-randomly generated seed is wanted)
@@ -75,9 +75,9 @@ class CFN:
         if vns is not None:
             tb2.option.vnsInitSol = vns   # if vns different than None then perform Decomposition-Guided Variable Neighborhood Search (-1: initial solution at random, -2: minimum domain values, -3: maximum domain values, -4: first solution found by DFS, >=0: or by LDS with at most vns discrepancies)
             tb2.option.lds = 4
-            tb2.option.restart = 10000;
-            tb2.option.searchMethod = 2;    # 0:DFBB or HBFS, 1:VNS, 2:DGVNS 4:Parallel DGVNS
-            tb2.option.vnsNeighborVarHeur = 3;   # 0: random, 1:conflict, 3: decomposition
+            tb2.option.restart = 10000
+            tb2.option.searchMethod = 2    # 0:DFBB or HBFS, 1:VNS, 2:DGVNS 4:Parallel DGVNS
+            tb2.option.vnsNeighborVarHeur = 3   # 0: random, 1:conflict, 3: decomposition
 
         self.Variables = {}
         self.VariableIndices = {}
@@ -163,7 +163,7 @@ class CFN:
             if isinstance(v, str):
                 v = self.VariableIndices.get(v, -1)
             if (v < 0 or v >= len(self.VariableNames)):
-                raise RuntimeError("Out of range variable index:"+str(v))
+                raise RuntimeError("Out of range variable index:"+str(v)+" for variable "+scope[i])
             iscope.append(v) 
             
         if (len(iscope) == 0 and isinstance(costs, (int, float))):
@@ -230,7 +230,7 @@ class CFN:
             if isinstance(v, str):
                 v = self.VariableIndices.get(v, -1)
             if (v < 0 or v >= len(self.VariableNames)):
-                raise RuntimeError("Out of range variable index:"+str(v))
+                raise RuntimeError("Out of range variable index:"+str(v)+" for variable "+scope[i])
             iscope.append(v) 
             
         if (len(iscope) == 0):
@@ -294,7 +294,7 @@ class CFN:
             if isinstance(v, str):
                 v = self.VariableIndices.get(v, -1)
             if (v < 0 or v >= len(self.VariableNames)):
-                raise RuntimeError("Out of range variable index:"+str(v))
+                raise RuntimeError("Out of range variable index:"+str(v)+" for variable "+scope[i])
             iscope.append(v) 
         sscope = set(iscope)
         if len(iscope) != len(sscope):
@@ -370,7 +370,7 @@ class CFN:
             if isinstance(v, str):
                 v = self.VariableIndices.get(v, -1)
             if (v < 0 or v >= len(self.VariableNames)):
-                raise RuntimeError("Out of range variable index:"+str(v))
+                raise RuntimeError("Out of range variable index:"+str(v)+" for variable "+scope[i])
             iscope.append(v) 
 
         if operand == '>=' or operand == '>' or operand == '==':
@@ -412,7 +412,7 @@ class CFN:
             if isinstance(v, str):
                 v = self.VariableIndices.get(v, -1)
             if (v < 0 or v >= len(self.VariableNames)):
-                raise RuntimeError("Out of range variable index:"+str(v))
+                raise RuntimeError("Out of range variable index:"+str(v)+" for variable "+scope[i])
             iscope.append(v)
         if (len(iscope) >= 2):
             if (encoding=='binary'):
@@ -421,17 +421,17 @@ class CFN:
                         costs = [(0 if (self.CFN.wcsp.toValue(iscope[i], a) != self.CFN.wcsp.toValue(iscope[j], b) or (excepted and ((self.CFN.wcsp.toValue(iscope[i], a) in excepted) or (self.CFN.wcsp.toValue(iscope[j], b) in excepted)))) else self.Top) for a in range(self.CFN.wcsp.getDomainInitSize(iscope[i])) for b in range(self.CFN.wcsp.getDomainInitSize(iscope[j]))]
                         self.CFN.wcsp.postBinaryConstraint(iscope[i], iscope[j], costs, incremental)
             elif (encoding=='salldiff'):
-                self.CFN.wcsp.postWAllDiff(iscope, "var", "flow", tb2.MAX_COST);
+                self.CFN.wcsp.postWAllDiff(iscope, "var", "flow", tb2.MAX_COST)
             elif (encoding=='salldiffdp'):
-                self.CFN.wcsp.postWAllDiff(iscope, "var", "DAG", tb2.MAX_COST);
+                self.CFN.wcsp.postWAllDiff(iscope, "var", "DAG", tb2.MAX_COST)
             elif (encoding=='salldiffkp'):
-                self.CFN.wcsp.postWAllDiff(iscope, "hard", "knapsack", tb2.MAX_COST);
+                self.CFN.wcsp.postWAllDiff(iscope, "hard", "knapsack", tb2.MAX_COST)
             elif (encoding=='salldiffeq'):
-                self.CFN.wcsp.postWAllDiff(iscope, "hardeq", "knapsack", tb2.MAX_COST);
+                self.CFN.wcsp.postWAllDiff(iscope, "hardeq", "knapsack", tb2.MAX_COST)
             elif (encoding=='salldiffge'):
-                self.CFN.wcsp.postWAllDiff(iscope, "hardge", "knapsack", tb2.MAX_COST);
+                self.CFN.wcsp.postWAllDiff(iscope, "hardge", "knapsack", tb2.MAX_COST)
             elif (encoding=='walldiff'):
-                self.CFN.wcsp.postWAllDiff(iscope, "hard", "network", tb2.MAX_COST);
+                self.CFN.wcsp.postWAllDiff(iscope, "hard", "network", tb2.MAX_COST)
             else:
                 raise RuntimeError("Unknown encoding for AllDifferent: "+encoding)
 
@@ -454,7 +454,7 @@ class CFN:
             if isinstance(v, str):
                 v = self.VariableIndices.get(v, -1)
             if (v < 0 or v >= len(self.VariableNames)):
-                raise RuntimeError("Out of range variable index:"+str(v))
+                raise RuntimeError("Out of range variable index:"+str(v)+" for variable "+scope[i])
             iscope.append(v)
         params = str(list(parameters))[1:-1].replace(',','').replace('\'','')
         self.CFN.wcsp.postGlobalFunction(iscope, gcname, params)
@@ -483,7 +483,7 @@ class CFN:
                 if (v < 0 or v >= len(self.VariableNames)):
                     v = self.AddVariable(vname, [(problem.CFN.wcsp.getValueName(i, value) if len(problem.CFN.wcsp.getValueName(i, value)) > 0 else value) for value in problem.Domain(vname)])
             if (v < 0 or v >= len(self.VariableNames)):
-                raise RuntimeError("Out of range variable index:"+str(v))
+                raise RuntimeError("Out of range variable index:"+str(v)+" for variable "+scope[i])
             iscope.append(v)
         multicfn = MultiCFN()
         multicfn.PushCFN(problem, -1)
@@ -965,7 +965,7 @@ class CFN:
             self.CFN.wcsp.increase(self.VariableIndices[var] if isinstance(var, str) else var, value)
             self.CFN.wcsp.propagate()
         else:
-            self.AddFunction([var], [0 if self.GetValue(var, index) >= value) else self.Top for index in range(self.GetDomainInitSize(var))])
+            self.AddFunction([var], [0 if self.GetValue(var, index) >= value else self.Top for index in range(self.GetDomainInitSize(var))])
         
     def Decrease(self, var, value):
         """Decrease removes the last values strictly greater than a given value in the domain of a variable.
@@ -979,7 +979,7 @@ class CFN:
             self.CFN.wcsp.decrease(self.VariableIndices[var] if isinstance(var, str) else var, value)
             self.CFN.wcsp.propagate()
         else:
-            self.AddFunction([var], [0 if self.GetValue(var, index) <= value) else self.Top for index in range(self.GetDomainInitSize(var))])
+            self.AddFunction([var], [0 if self.GetValue(var, index) <= value else self.Top for index in range(self.GetDomainInitSize(var))])
             
         
     def Deconnect(self, var):
