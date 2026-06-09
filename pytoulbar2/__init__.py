e6ffdcd452fd2d63b66242241110f795ce5a9640
Samuel Buchet <samuel.buchet@inrae.fr>
2026-06-09 08:58:35 +0200

    [release] Added version file for release 1.3.0

--- pytoulbar2/__init__.py
+++ pytoulbar2/__init__.py
@@ -1,5 +1,5 @@
 from .pytoulbar2 import *
 
-__version__ = ""
-__toulbar2_version__ = ""
-__wrapper_version__ = ""
+__version__ = "1.3.0.0" # hash 4c614df9cfafed21d7399798efeade47bf3b3037 
+__toulbar2_version__ = "1.3.0"
+__wrapper_version__ = "0"
--- src/ToulbarVersion.hpp
+++ src/ToulbarVersion.hpp
@@ -1,2 +1,2 @@
 // Cmake generated version
-#define Toulbar_VERSION "v1.2.2.1-11-g9c3b4526-master (1780935031)"
+#define Toulbar_VERSION "v1.2.2.1-15-g4c614df9-master (1780988315)"
