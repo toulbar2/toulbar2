333ce1bf4b142ea599c069b8a0cad75869794bfa
Samuel Buchet <samuel.buchet@inrae.fr>
2026-06-09 10:35:38 +0200

    [misc] reset pytb2 init

--- pytoulbar2/__init__.py
+++ pytoulbar2/__init__.py
@@ -1,5 +1,5 @@
 from .pytoulbar2 import *
 
-__version__ = "1.3.0.0" # hash 96244a9a6fd39a42676f7e24c3d3c3f8c3ca02e5 
-__toulbar2_version__ = "1.3.0"
-__wrapper_version__ = "0"
+__version__ = ""
+__toulbar2_version__ = ""
+__wrapper_version__ = ""
