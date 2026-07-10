44619bafbfcfc8b609f560b2805c2497e68731aa
Samuel Buchet <samuel.buchet@inrae.fr>
2026-07-10 12:25:54 +0200

    [misc] Updated version file on push

--- src/ToulbarVersion.hpp
+++ src/ToulbarVersion.hpp
@@ -1,2 +1,2 @@
 // Cmake generated version
-#define Toulbar_VERSION "v1.3.0-22-gc36367f5-pytoulbar2-tainted (1783673773)"
+#define Toulbar_VERSION "v1.3.0-24-gb558d04c-pytoulbar2-tainted (1783679153)"
