cc9e09111d9ef9d3ebd2484cab2c5acb73f778a3
Samuel Buchet <samuel.buchet@inrae.fr>
2026-07-13 14:54:40 +0200

    update pytoulbar2 tensor example

--- validation/default/tensor_costs.py
+++ validation/default/tensor_costs.py
@@ -4,24 +4,52 @@ import numpy as np
 
 np.random.seed(42)
 
-n_var = 10
+n_var = 8
+dom_size = 3
 scopes = [[i,j] for j in range(n_var) for i in range(n_var) if i < j]
 
-binary_costs = np.random.rand(len(scopes), 3, 3)
-print(binary_costs)
+# test with several cost tables
+binary_costs = np.random.rand(len(scopes), dom_size, dom_size)
 
 # classical way to create the problem
 model = tb2.CFN()
 for i in range(10):
-    model.AddVariable('x_'+str(i), range(3))
+    model.AddVariable('x_'+str(i), range(dom_size))
 for ind, scope in enumerate(scopes):
     model.AddFunction(scope, binary_costs[ind].flatten())
-sol = model.Solve()
-print(sol)
+sol1 = model.Solve()
 
 # creation of the problem through tensors
 model = tb2.CFN()
-model.CFN.wcsp.makeEnumeratedVariableVec(10, 'x', 0, 2)
+model.CFN.wcsp.makeEnumeratedVariableVec(10, 'x', 0, dom_size-1)
 model.CFN.wcsp.postMultBinaryVecConstraints(np.array(scopes), binary_costs)
-sol = model.Solve()
-print(sol)
+sol2 = model.Solve()
+
+assert(abs(sol1[1]-sol2[1]) <= 1e-3)
+
+
+
+
+
+# test with a unique cost table
+binary_costs = np.random.rand(dom_size, dom_size)
+unary_costs  = np.random.rand(n_var, dom_size)
+
+# classical way to create the problem
+model = tb2.CFN()
+for i in range(10):
+    model.AddVariable('x_'+str(i), range(dom_size))
+for ind, scope in enumerate(scopes):
+    model.AddFunction(scope, binary_costs.flatten())
+for var_ind in range(n_var):
+    model.AddFunction([var_ind], unary_costs[var_ind])
+sol1 = model.Solve()
+
+# creation of the problem through tensors
+model = tb2.CFN()
+model.CFN.wcsp.makeEnumeratedVariableVec(10, 'x', 0, dom_size-1)
+model.CFN.wcsp.postBinaryVecConstraints(np.array(scopes), binary_costs)
+model.CFN.wcsp.postUnaryVecConstraints(np.arange(n_var), unary_costs)
+sol2 = model.Solve()
+
+assert(abs(sol1[1]-sol2[1]) <= 1e-3)
