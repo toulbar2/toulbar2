a55cd838034cc40512cd39eb4ad775b8e3929c35
Samuel Buchet <samuel.buchet@inrae.fr>
2026-08-20 17:40:32 +0200

    [misc] Updated version file on push

--- src/ToulbarVersion.hpp
+++ src/ToulbarVersion.hpp
@@ -1,2 +1,2 @@
 // Cmake generated version
-#define Toulbar_VERSION "v1.2.1-510-g3cbad426-pytb2_tensor-tainted (1787228385)"
+#define Toulbar_VERSION "v1.2.1-512-g881420e9-pytb2_tensor-tainted (1787240432)"
